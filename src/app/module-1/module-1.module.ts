f714494e2431effcedcbd90846625b14aade6a29
Ngoc Tho Nguyen <thofreising141@gmail.com>
2022-07-24 12:24:53 +0000

    using subject

--- src/app/module-1/module-1.module.ts
+++ src/app/module-1/module-1.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { Component1Component } from './component-1/component-1.component';
 import { Module1RoutingModule } from './module-1-routing/module-1-routing.module';
-import {DropdownModule} from 'primeng/dropdown';
-import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Component2Component } from './component-2/component-2.component';
 
 @NgModule({
-  imports: [Module1RoutingModule, DropdownModule, FormsModule, BrowserModule, BrowserAnimationsModule],
-  declarations: [Component1Component],
+  imports: [Module1RoutingModule],
+  declarations: [Component1Component, Component2Component],
 })
 export class Module1Module {}
