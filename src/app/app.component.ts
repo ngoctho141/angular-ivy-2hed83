ed33b3cce589d69179c38deb42d0403d3c490a8c
Ngoc Tho Nguyen <thofreising141@gmail.com>
2023-01-31 11:06:58 +0000

    add angular config for 3rd source to debug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,15 @@
 import { Component, VERSION } from '@angular/core';
+import { plainToClass } from 'class-transformer';
 import {MenuItem} from 'primeng/api';
 import { JsonIntersectionService } from './json-intersection.service';
+
+class A {
+  deviceId:string;
+  deviceName:string;
+  getName(){
+    return this.deviceName;
+  }
+}
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
@@ -24,6 +33,10 @@ export class AppComponent  {
   }
 
     ngOnInit() {
-
+      var a = {
+        deviceId: '2414',
+        deviceName: "Koralin Druck"
+      }
+      console.log(plainToClass(A, a).getName());
     }
 }
