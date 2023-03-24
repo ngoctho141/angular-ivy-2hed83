96708b0b18f3b92ac6ac59086a73d791124609f6
Ngoc Tho Nguyen <thofreising141@gmail.com>
2023-03-24 13:01:15 +0000

    AG-24.03: create library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import {ContextMenuModule} from 'primeng/contextmenu';
+import { ButtonModule } from "@mycomp/ui-sdk/button";
 
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, AppRoutingModule, ContextMenuModule],
+  imports: [BrowserModule, FormsModule, AppRoutingModule, ContextMenuModule, ButtonModule],
   declarations: [AppComponent, HelloComponent],
   bootstrap: [AppComponent],
 })
